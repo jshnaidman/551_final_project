a8e589fb51efc401bfc91f57f3f056741e7e1ddb
BarleenKaur <barleenk@gmail.com>
2018-04-08 17:51:49 -0400

    added amsgrad.py

---
+++ amsgrad.py
@@ -0,0 +1,82 @@
+# The below code modifies pytorch implementation of Adams optimizer to implement Amsgrad algorithm 
+
+class Amsgrad(Optimizer):
+    """Implements Amsgrad algorithm.
+
+    It has been proposed in `On the Convergence of Adam and Beyond`_.
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+
+    .. _n the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
+    """
+
+    # constructor to initialize the hyper-parameters
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+                 weight_decay=0):
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay)
+        super(Amsgrad, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('Amsgrad does not support sparse gradients, please consider SparseAdam instead')
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    # Maximum exponential moving average of squared gradient values
+                    state['exp_avg_sq_maximum'] = torch.zeros_like(p.data)
+                exp_avg, exp_avg_sq, exp_avg_sq_maximum = state['exp_avg'], state['exp_avg_sq'], state['exp_avg_sq_maximum']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                
+                # Stores the maximum of previous maximum exponential average of squared gradients and current value
+                exp_avg_sq_maximum = torch.max(exp_avg_sq_maximum,exp_avg_sq)
+                
+                
+                denom = exp_avg_sq_maximum.sqrt().add_(group['eps'])
+
+                step_size = group['lr']
+
+                # Performs update on parameters of the network based on gradients
+                p.data.addcdiv_(-step_size, exp_avg, denom)
+
+        return loss
